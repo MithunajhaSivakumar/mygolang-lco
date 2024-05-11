8c68bb9406a90d697cc2faa7b4e25aa305260570
MithunajhaS <mithunajhasivakumar2002@gmail.com>
2024-05-11 14:21:49 +0530

    Variables concepts

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Outside cannot use no var style
+// jwtToken := 1000
+
+// var name start with Uppercase - public variable
+const LoginToken string = "abcdefgh"
+
 func main() {
 	var username string = "Mithunajha"
 	fmt.Println(username)
@@ -11,7 +17,7 @@ func main() {
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable is of type : %T \n", isLoggedIn)
 
-	var smallVal uint8 = 255 
+	var smallVal uint8 = 255
 	fmt.Println(smallVal)
 	fmt.Printf("Variable is of type : %T \n", smallVal)
 
@@ -20,10 +26,18 @@ func main() {
 	fmt.Printf("Variable is of type : %T \n", smallFloat)
 
 	// default values and some aliases
-	var anotherVariable int 
+	var anotherVariable int
 	fmt.Println(anotherVariable)
 	fmt.Printf("Variable is of type: %T \n", anotherVariable)
 
-	// implicit way of declaring varible
-	// var website = "Learn"
+	// implicit way of declaring variable
+	var website = "Learning"
+	fmt.Println(website)
+
+	// no var style
+	numberOfUser := 100
+	fmt.Println(numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
 }
